de78952c4465ab736231d35f91647139cedb2f24
Ertan YILDIZ <ertanyildizz@gmail.com>
2017-11-04 17:28:35 +0300

    Update generate.js

--- generate.js
+++ generate.js
@@ -1,5 +1,5 @@
 $("form :input").each(function(){
    var input = $(this);
     var name = input.attr("name");
-    $('#my_div').append("$"+name+"= $this->input->post('"+name+"');<br>"  );
+    $('#my_div').append("$"+name+" = $this->input->post('"+name+"');<br>"  );
 });
